df807d392c9e27f13e60777b94b27e6b14d2052e
cat67315 <ezpurplecat@gmail.com>
2025-10-30 07:46:30 -0600

    Implement main functionality and setup script for brainrot notifier

--- setup.bat
+++ setup.bat
@@ -1,6 +1,16 @@
 @echo off
-winget install Python.Python.3
-pip install -r requirements.txt
-winget install --id UB-Mannheim.TesseractOCR -e
-echo Finished install!
-pause
+pip --version
+if %errorlevel% neq 0 (
+    echo pip is not installed, run the pithon installer first.
+    echo Make shure to check the box that says "Add Python to PATH" and the box that says "Install pip"
+    echo Downloading python installer...
+    curl https://www.python.org/ftp/python/3.13.9/python-3.13.9-amd64.exe
+    echo Starting python installer in 5 seconds...
+    timeout /t 5 /nobreak >nul
+    start python-3.13.9-amd64.exe
+) else (
+    pip install -r requirements.txt
+    winget install --id UB-Mannheim.TesseractOCR -e
+    echo Finished install! Run main.py to start the program.
+    pause
+)
